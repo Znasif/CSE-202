0407f7f70c295e37f07bf05cfbb870fdf4684b60
Znasif <1305030.nz@ugrad.cse.buet.ac.bd>
2015-11-28 11:15:18 +0600

    Update 600B.py

--- 600B.py
+++ 600B.py
@@ -1,12 +1,18 @@
-x1, y1, r1=input().split()
-x1, y1, r1=int(x1),int(y1),int(r1)
-x2, y2, r2=input().split()
-x2, y2, r2=int(x2),int(y2),int(r2)
+x1,y1,r1=input().split()
+x1,y1,r1=int(x1),int(y1),int(r1)
+x2,y2,r2=input().split()
+x2,y2,r2=int(x2),int(y2),int(r2)
 
 def dis(a1,a2,b1,b2):
     return ((a1-b1)*(a1-b1)+(a2-b2)*(a2-b2))**.5
 
 d=dis(x1,y1,x2,y2)
+p=math.pi
 
-if(d>r1+r2) print(0)
-elif()
+
+if(d>r1+r2):
+    print(0)
+elif(d==0 and r1==r2):
+    print(p*r1*r1)
+else:
+    
